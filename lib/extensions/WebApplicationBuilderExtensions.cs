a9ec4ddce20a97ac78b217e9b60ab528f1e92760
kevin-presalytics <kevin@presalytics.io>
2023-05-10 09:37:42 -0700

    add enviroment variables to configuration

--- lib/extensions/WebApplicationBuilderExtensions.cs
+++ lib/extensions/WebApplicationBuilderExtensions.cs
@@ -16,6 +16,7 @@ namespace lib.extensions
             builder.Configuration.AddYamlFile(Path.Join(workingDir, "appsettings.default.yaml"), optional: false, reloadOnChange: true);
             builder.Configuration.AddYamlFile(Path.Join(workingDir, "appsettings.yaml"), optional: true, reloadOnChange: true);
             builder.Configuration.AddYamlFile(Path.Join(workingDir, "appsettings.local.yaml"), optional: true, reloadOnChange: true);
+            builder.Configuration.AddEnvironmentVariables();
             AppConfiguration appConfiguration = new AppConfiguration();
             builder.Configuration.Bind(appConfiguration);
             builder.Services.AddSingleton<AppConfiguration>(appConfiguration);
